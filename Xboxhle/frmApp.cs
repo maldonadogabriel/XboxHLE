4e1dd9bbc56c681849418ec500c3a1a8827df074
Gabriel Maldonado <maldonadogabriel@outlook.com>
2015-09-11 20:36:46 -0400

    XboxHLE

---
+++ Xboxhle/frmApp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace xboxhle
+{
+    public partial class frmApp : Form
+    {
+
+        public frmApp()
+        {
+            InitializeComponent();
+        }
+
+        public string ThisText
+        {
+            get
+            {
+                return this.Text;
+            }
+            set
+            {
+                this.Text = value;
+            }
+        }
+
+        private void frmApp_Load(object sender, EventArgs e)
+        {
+            this.Text = Application.ProductName + " Application Log";
+            int interval = 0;
+            frmMain MainFrm = new frmMain();
+            while (this.Visible == true)
+            {
+                if (interval == 1)
+                {
+                    if (this.Visible == true)
+                    {
+                        MainFrm.Show();
+                        break;
+                    }
+                }
+                interval++;
+            }
+            
+        }
+    }
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox box, string text, Color fcolor, Color bcolor)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+            box.SelectionColor = fcolor;
+            box.SelectionBackColor = bcolor;
+            
+            box.AppendText(text);
+        }
+    }
+}
---
+++ Xboxhle/xbox/video.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+namespace xboxhle
+{
+    public class video
+    {
+        public static void vmem_read(PaintEventArgs e)
+        {
+            Graphics displayGraphics = e.Graphics;
+            Bitmap tmpBmp = new Bitmap(640, 480);
+
+            BitmapData Locked = tmpBmp.LockBits(new Rectangle(0, 0, 640, 480), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+            unsafe
+            {
+                UInt32* v = (UInt32*)Locked.Scan0;
+
+                for (uint y = 0; y < 480; ++y)
+                {
+                    for (uint x = 0; x < 640; ++x)
+                    {
+                        if (i386.framebuffer.Length != 0) { *v++ = i386.framebuffer[x + y * 640 * 4]; }
+                    }
+                }
+            }
+            
+            tmpBmp.UnlockBits(Locked);
+            displayGraphics.DrawImageUnscaled(tmpBmp, new Rectangle(0, 0, 640, 480)); 
+        }
+        public static void debugInfo(PaintEventArgs e)
+        {
+            // Create string to draw.
+            String drawString =
+                "registers \n" +
+                "reg32_eax:" + xboxhle.i386.reg32_eax.ToString("X") + " " + "reg32_ebx:" + xboxhle.i386.reg32_ebx.ToString("X") + "\n" +
+                "reg32_ecx:" + xboxhle.i386.reg32_ecx.ToString("X") + " " + "reg32_edx:" + xboxhle.i386.reg32_edx.ToString("X") +
+                "\n\n" + "src/des" +
+                "\n" + "reg32_esi:" + xboxhle.i386.reg32_esi.ToString("X") + " " + "reg32_edi:" + xboxhle.i386.reg32_edi.ToString("X") +
+                "\n\n" + "pointers" +
+                "\n" + "reg32_ebp:" + xboxhle.i386.reg32_ebp.ToString("X") + " " + "reg32_esp:" + xboxhle.i386.reg32_esp.ToString("X") +
+                "\n\n" + "segments" +
+                "\n" + "seg16_cs:" + xboxhle.i386.seg16_cs.ToString("X") + " " + "seg16_ds:" + xboxhle.i386.seg16_ds.ToString("X") +
+                "\n" + "seg16_es:" + xboxhle.i386.seg16_es.ToString("X") + " " + "seg16_ss:" + xboxhle.i386.seg16_ss.ToString("X");
+
+            // Create font and brush.
+            Font drawFont = new Font("Consolas", 8);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            // Create point for upper-left corner of drawing.
+            PointF drawPoint = new PointF(0, 0);
+
+            // Draw string to screen.
+            e.Graphics.DrawString(drawString, drawFont, drawBrush, drawPoint);
+        }
+    }
+}
