9b78c6fe1169d27e1a354ef1369b68a056646ad6
Gabriel-Maldonado <Gabriel-Maldonado@users.noreply.github.com>
2015-09-20 15:45:03 -0400

    Update table.cs

--- Xboxhle/xbox/profiler/table.cs
+++ Xboxhle/xbox/profiler/table.cs
@@ -1,6 +1,22 @@
-// © Copyright 2015  Gabriel Maldonado.  All Rights Reserved.
-// Linkedin: https://www.linkedin.com/pub/gabriel-maldonado/63/457/866
-// Github: https://github.com/Gabriel-Maldonado/XboxHLE
+/*
+Copyright (C) 2015  Gabriel Maldonado
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+* Linkedin: https://www.linkedin.com/pub/gabriel-maldonado/63/457/866
+* Github: https://github.com/Gabriel-Maldonado/XboxHLE
+*/
 
 using System;
 using System.Text;
