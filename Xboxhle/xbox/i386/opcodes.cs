db6dade863cfc07267e3362412575fb5879b434a
Gabriel-Maldonado <Gabriel-Maldonado@users.noreply.github.com>
2015-09-21 13:48:18 -0400

    Update opcodes.cs

--- Xboxhle/xbox/i386/opcodes.cs
+++ Xboxhle/xbox/i386/opcodes.cs
@@ -19,10 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace xboxhle.xbox.i386
 {
